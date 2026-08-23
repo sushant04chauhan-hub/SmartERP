9427f9e4616e84d8b123c32d983c3e0f02b1ae30
Sushant Chauhan <sushant.04.chauhan@gmail.com>
2026-08-23 23:11:10 +0530

    Add expense edit and delete functionality

--- finance/urls.py
+++ finance/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import expense_list
+from .views import expense_create, expense_list, expense_edit, expense_delete
 
 
 urlpatterns = [
@@ -8,5 +8,22 @@ urlpatterns = [
         "",
         expense_list,
         name="expense_list",
+    ),
+
+    path(
+        "add/",
+        expense_create,
+        name="expense_create",
+    ),
+    path(
+        "<int:pk>/edit/",
+        expense_edit,
+        name="expense_edit",
+    ),
+
+    path(
+        "<int:pk>/delete/",
+        expense_delete,
+        name="expense_delete",
     ),
 ]
--- finance/views.py
+++ finance/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
 
 from .models import Expense
 
@@ -16,5 +16,70 @@ def expense_list(request):
         "finance/expense_list.html",
         {
             "expenses": expenses,
+        },
+    )
+
+
+@login_required
+def expense_create(request):
+
+    if request.method == "POST":
+
+        Expense.objects.create(
+            title=request.POST.get("title"),
+            category=request.POST.get("category"),
+            amount=request.POST.get("amount"),
+            expense_date=request.POST.get("expense_date"),
+            description=request.POST.get("description"),
+        )
+
+        return redirect("expense_list")
+
+    return render(
+        request,
+        "finance/expense_form.html",
+    )
+
+
+@login_required
+def expense_edit(request, pk):
+
+    expense = get_object_or_404(Expense, pk=pk)
+
+    if request.method == "POST":
+
+        expense.title = request.POST.get("title")
+        expense.category = request.POST.get("category")
+        expense.amount = request.POST.get("amount")
+        expense.expense_date = request.POST.get("expense_date")
+        expense.description = request.POST.get("description")
+
+        expense.save()
+
+        return redirect("expense_list")
+
+    return render(
+        request,
+        "finance/expense_form.html",
+        {
+            "expense": expense,
+        },
+    )
+
+
+@login_required
+def expense_delete(request, pk):
+
+    expense = get_object_or_404(Expense, pk=pk)
+
+    if request.method == "POST":
+        expense.delete()
+        return redirect("expense_list")
+
+    return render(
+        request,
+        "finance/expense_confirm_delete.html",
+        {
+            "expense": expense,
         },
     )
